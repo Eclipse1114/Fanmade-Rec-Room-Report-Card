3bc3dca2e6cd12b715dd7ef17118bd0707177445
Eclipse <64746@thedps.org>
2026-08-20 16:00:21 -0400

    Update HTML styling and structure in card generation

--- code.py
+++ code.py
@@ -7,27 +7,31 @@ final_activity = st.text_input("When did you stop playing?")
 submit = st.button("Generate Card")
 
 if submit:
-st.html(f"""
-<style>
-    .card {
-        color: white;
-    }
-    .card h1 {
-        color: orange;
-    }
-</style>
+    st.html(f"""
+    <style>
+        .stApp {
+            background-color: black;
+            text-align: center;
+        }
+        .card {
+            color: white;
+        }
+        .card h1 {
+            color: orange;
+        }
+    </style>
 
-<div class="card">
-    <h1>Rec Room Final Report Card</h1>
-    <h2>@{name}</h2>
+    <div class="card">
+        <h1>Rec Room Final Report Card</h1>
+        <h2>@{name}</h2>
 
-    <h3>Player Since:</h3>
-    <p>{joined}</p>
+        <h3>Player Since:</h3>
+        <p>{joined}</p>
 
-    <h3>Final Activity:</h3>
-    <p>{final_activity}</p>
+        <h3>Final Activity:</h3>
+        <p>{final_activity}</p>
 
-    <h3>Subscribers:</h3>
-    <p>{subs}</p>
-</div>
-""")
+        <h3>Subscribers:</h3>
+        <p>{subs}</p>
+    </div>
+    """, unsafe_allow_html=True)
